c69b66b986fa1999549637ff2b6b46509c4e1195
alopao9511 <113046679+alopao9511@users.noreply.github.com>
2022-09-14 13:53:04 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+from os import sep
+from turtle import end_fill
+
+
+edad = 20
+if edad>18:
+print("menor  de edad")
+elif: edad<=0:
+    print("tu edad no puede ser 0")
+    else:
+        "menor de edad"
+
+print("comienzo")
+for i in [0,1,2,3,4]:
+    print("hola", end ="?n")
+    print()
+        print("final")
+    
+     
+
+
+
+
+
